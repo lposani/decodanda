f235bf26d1cd9b7f8970fd322fc1a9f1b3d75885
lposani <lorenzo.posani@gmail.com>
2023-01-15 17:23:35 -0500

    Updated init.

--- decodanda/__init__.py
+++ decodanda/__init__.py
@@ -1,4 +1,4 @@
-from .classes import Decodanda, decoding_analysis, balance_decodandas
+from .classes import Decodanda, decoding_analysis, balance_decodandas, check_session_requirements
 from .in_time import decode_in_time, CCGP_in_time
 from .visualize import *
 from .utilities import *
